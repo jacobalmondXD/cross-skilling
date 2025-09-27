66b512bae1974fbea3c08d2d9f7849ff70d74a75
Jacob John Almond <jacob.almond@createfuture.com>
2025-09-27 17:48:56 +0100

    - Implement typescript Cypress config file and dependencies

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,6 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from "cypress";
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
     excludeSpecPattern: [
       "cypress/e2e/1-getting-started/**/*",
