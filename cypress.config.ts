fa4d6dc8a733fc99f303ac12af6073877deb3ae5
Jacob John Almond <jacob.almond@createfuture.com>
2025-09-30 08:52:44 +0100

    Add esbuild preprocessor and update dependencies to complete Cypress preprocessor integration

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from "cypress";
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
 
 export default defineConfig({
   e2e: {
@@ -7,8 +10,21 @@ export default defineConfig({
       "cypress/e2e/1-getting-started/**/*",
       "cypress/e2e/2-advanced-examples/**/*",
     ],
-    setupNodeEvents(on, config) {
+    async setupNodeEvents(on, config) {
       // implement node event listeners here
+      // Add the Cucumber preprocessor plugin
+      await addCucumberPreprocessorPlugin(on, config);
+
+      // Use esbuild for bundling
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+
+      // Return the updated config
+      return config;
     },
   },
 });
