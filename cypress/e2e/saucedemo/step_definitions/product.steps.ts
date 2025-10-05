ac5621c1a6f78e7e7ed487aa68088e226e3f8c52
Jacob John Almond <jacob.almond@createfuture.com>
2025-10-05 12:54:06 +0100

    Implement fixtures for product and shipping test data

--- cypress/e2e/saucedemo/step_definitions/product.steps.ts
+++ cypress/e2e/saucedemo/step_definitions/product.steps.ts
@@ -7,13 +7,36 @@ import CheckoutInformationPage from "../pages/CheckoutInformationPage";
 import CheckoutOverviewPage from "../pages/CheckoutOverviewPage";
 import CheckoutCompletePage from "../pages/CheckoutCompletePage";
 
+interface ProductData {
+  id: string;
+  name: string;
+}
+
+interface ShippingData {
+  firstName: string;
+  lastName: string;
+  postalCode: string;
+}
+
+let productData: { [key: string]: ProductData };
+let shippingData: { [key: string]: ShippingData };
+
+before(() => {
+  cy.fixture("products.json").then((data) => {
+    productData = data;
+  });
+  cy.fixture("shipping.json").then((data) => {
+    shippingData = data;
+  });
+});
+
 Given("I am a logged-in user on the product catalog page", () => {
   cy.login(username);
   cy.visit("/inventory.html", { failOnStatusCode: false });
 });
 
 When("I click the Add to cart button for a chosen item", () => {
-  ProductPage.clickAddToCart("sauce-labs-backpack");
+  ProductPage.clickAddToCart(productData.itemOne.id);
 });
 
 Then("the shopping cart icon should show 1", () => {
@@ -30,7 +53,7 @@ Then("I should be navigated to the Your Cart page", () => {
 });
 
 Then("I should see the selected item listed in my cart", () => {
-  CartPage.cartItem.should("have.text", "Sauce Labs Backpack");
+  CartPage.cartItem.should("have.text", productData.itemOne.name);
 });
 
 When("I click the Checkout button", () => {
@@ -46,9 +69,9 @@ Then("I should be navigated to the Checkout: Your Information page", () => {
 });
 
 When("I enter valid shipping information", () => {
-  CheckoutInformationPage.enterFirstName("John");
-  CheckoutInformationPage.enterLastName("Smith");
-  CheckoutInformationPage.enterPostalCode("AB12 1AB");
+  CheckoutInformationPage.enterFirstName(shippingData.personOne.firstName);
+  CheckoutInformationPage.enterLastName(shippingData.personOne.lastName);
+  CheckoutInformationPage.enterPostalCode(shippingData.personOne.postalCode);
 });
 
 When("I click the Continue button", () => {
@@ -64,7 +87,10 @@ Then("I should be navigated to the Checkout: Overview page", () => {
 });
 
 When("I review the order summary", () => {
-  CheckoutOverviewPage.checkoutItem.should("have.text", "Sauce Labs Backpack");
+  CheckoutOverviewPage.checkoutItem.should(
+    "have.text",
+    productData.itemOne.name
+  );
   CheckoutOverviewPage.paymentInformation.should(
     "have.text",
     "SauceCard #31337"
